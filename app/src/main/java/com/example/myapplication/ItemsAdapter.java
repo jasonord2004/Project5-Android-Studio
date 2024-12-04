14c1f1284bc36e17d9ccd8f98d052fb7fd9b59e5
jasonord <jason.ord2004@gmail.com>
2024-12-03 20:24:06 -0500

    Edited ItemsAdapter
    Changed ItemsAdapter Signature

--- app/src/main/java/com/example/myapplication/ItemsAdapter.java
+++ app/src/main/java/com/example/myapplication/ItemsAdapter.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemsHolder>{
-}
-
-public
+//public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemsHolder>{
+//}
+//
+//public static class ItemsHolder extends RecyclerView.ViewHolder {
+//    private TextView
+//}
