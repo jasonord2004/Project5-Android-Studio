feb1de3e46da134ec843bf517a0d2c7c3f1f2c75
jasonord <jason.ord2004@gmail.com>
2024-12-06 22:58:28 -0500

    Added some Javadoc Comments
    Added comments to some java files for the javadoc

--- app/src/main/java/com/example/myapplication/PizzasList.java
+++ app/src/main/java/com/example/myapplication/PizzasList.java
@@ -2,12 +2,31 @@ package com.example.myapplication;
 
 import java.util.ArrayList;
 
+/**
+ * This Java class follows the "Singleton Design Pattern". It creates a private instance of
+ * the arraylist of pizzas that can accessed in multiple activities.
+ * @author Jason Ordonez, Ankit Mithbavkar
+ */
 public class PizzasList {
-    private ArrayList<Pizza> pizzas;
+    /**
+     * The arraylist of pizzas in the current order
+     */
+    final private ArrayList<Pizza> pizzas;
 
+    /**
+     * The private instance of PizzasList that creates the new arraylist of pizzas
+     */
     private PizzasList(){ pizzas = new ArrayList<Pizza>(); }
 
+    /**
+     * The PizzasList of pizzas
+     */
     private static PizzasList pizzasList;
+
+    /**
+     * Returns the pizzaslist of pizzas. Creates one if it is null.
+     * @return - Returns the PizzasList
+     */
     public static PizzasList get(){
         if (pizzasList == null) {
             pizzasList = new PizzasList();
@@ -15,8 +34,10 @@ public class PizzasList {
         return pizzasList;
     }
 
-    public void addPizza(Pizza pizza) { pizzas.add(pizza); }
-    public void removePizza(Pizza pizza){ pizzas.remove(pizza); }
+    /**
+     * Returns the arraylist of pizzas. This will allow users to edit the values in the list.
+     * @return - Returns the arraylist of pizzas
+     */
     public ArrayList<Pizza> getPizzas() {
         return pizzas;
     }
